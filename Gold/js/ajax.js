22b79c3063b086b350205cb4f632ed056027d340
Ryan Dittmer <rdittmer@fullsail.edu>
2012-07-12 22:41:38 -0400

    tried to get ajax to work properly, no luck

---
+++ Gold/js/ajax.js
@@ -0,0 +1,25 @@
+$( '#remoteData' ).live( 'pageinit', function(){
+  $( '#jsonButton' ).on( "click", function () {
+        $.ajax( {
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success:function ( response ) {
+                for ( var i = 0, len = response.json.length; i < len; i++ ) {
+                    var item = response.json[i];
+					console.log(item);
+                    $( ' ' + 
+					'<div class="times">' +
+					'<p>' + item.Options + '</p>' +
+					'<p>' + item.reservist + '</p>' +
+					'<p>' + item.numberGames + '</p>' +
+					'<p>' + item.location + '</p>' +
+					'<p>' + item.date + '</p>' +
+					'<p>' + item.notes + '</p>' +
+					'</div>'
+					).appendTo( '#jsonData' );
+                }
+            }
+        });
+    });
+});
